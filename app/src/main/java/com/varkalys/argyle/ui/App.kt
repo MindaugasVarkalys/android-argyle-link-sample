84ca6744490cba16629ff94e3987b089fab729e4
Mindaugas Varkalys <varkalys.mindaugas@gmail.com>
2022-12-21 13:01:55 +0200

    Create project

---
+++ app/src/main/java/com/varkalys/argyle/ui/App.kt
@@ -0,0 +1,12 @@
+package com.varkalys.argyle.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.varkalys.argyle.ui.theme.AppTheme
+
+@Composable
+fun App() {
+    AppTheme {
+        Text("Hello Argyle!")
+    }
+}
---
+++ app/src/main/java/com/varkalys/argyle/ui/theme/Color.kt
@@ -0,0 +1,2 @@
+package com.varkalys.argyle.ui.theme
+
